25bff23fac657e99bcf335625d70a06e8be5d66b
Anjali kushwaha <kushwahanjali730@gmail.com>
2024-10-26 23:41:10 +0530

    koma

--- src/store/reducers/personSlice.jsx
+++ src/store/reducers/personSlice.jsx
@@ -2,11 +2,11 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-  info: null
+  info: null,
 }
 
 export const personSlice = createSlice({
-    name: 'person',
+    name: "person",
     initialState,
     reducers: {
      loadperson:(state,action) =>{
@@ -14,11 +14,11 @@ export const personSlice = createSlice({
      },
      removeperson:(state,action) =>{
         state.info = null;
-     }
+     },
     },
-  })
+  });
   
   // Action creators are generated for each case reducer function
   export const { loadperson,removeperson} = personSlice.actions;
   
-  export default personSlice.reducer
+  export default personSlice.reducer;
